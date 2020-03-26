2d5fcd31d033e370ad34e465f43576c6ac34c43f
张天华 <zhangtianhua@zhaojiafang.com>
2020-03-26 10:32:51 +0800

    更改内容

--- src/component/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  cssList: [
-    { id: 'css1', desc: '什么是BFC' },
-    { id: 'css2', desc: '谈下渐进增强和优雅晋级' },
-  ]
-}
--- src/component/home/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Home() {
-  return (
-    <div>这个是首页面</div>
-  )
-}
-
-export default Home;
